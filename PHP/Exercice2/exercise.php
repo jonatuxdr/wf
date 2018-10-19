86d1c3592d6a0cdcc1691170c3c21c9c6ab68b35
jonatuxdr <jonathan.deriemaecker@gmail.com>
2018-10-19 10:48:58 +0200

    You did sh*****

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -2,4 +2,13 @@
 $password;
 $salt;
 
-$saltedPassword = '';
+
+$firstPart = substr(
+    $password,
+    0,
+    floor(strlen($password)/2) + (strlen($password)%2)
+);
+
+$lastPart = substr($password, ceil(strlen($password)/ 2));
+//abc
+$saltedPassword = $firstPart . $salt . $lastPart;
