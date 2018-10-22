d2aa941b2fb17f980ac6136a3e0234e14f1671e7
jonatuxdr <jonathan.deriemaecker@gmail.com>
2018-10-22 14:21:11 +0200

    changes exo6

--- PHP/Exercice6/exercise.php
+++ PHP/Exercice6/exercise.php
@@ -1,1 +1,25 @@
 <?php
+
+function easterReverse($filePath) {
+  $fileContent = "";
+  $file = fopen($filePath, 'r+');
+  
+  while ($char=fgetc($file)) {
+      $fileContent .= $char;
+  }
+  
+  $middle = floor(strlen($fileContent)/2);
+  var_dump($middle, ftell($file));
+  
+  $reverseContent = "";
+  fseek($file, $middle ,SEEK_SET);
+  
+  while ($char = fgetc($file)) {
+      $reverseContent.=$char;
+  }
+  
+  $reverseContent = strrev($reverseContent);
+  fseek($file, $middle ,SEEK_SET);
+  fwrite($file, $reverseContent);
+  
+}
