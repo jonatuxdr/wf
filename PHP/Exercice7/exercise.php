5fea6516da9c1613da881a8108c86d3ada0dc927
jonatuxdr <jonathan.deriemaecker@gmail.com>
2018-10-22 17:10:07 +0200

    changes*

--- PHP/Exercice7/exercise.php
+++ PHP/Exercice7/exercise.php
@@ -1,1 +1,27 @@
 <?php
+
+function divide(int $num, int $denom) : float {
+    if ($denom === 0) {
+        throw new RuntimeException('Division by 0 is not allowed');
+        
+    }
+    return $num/$denom;
+}
+
+
+function arrayDivide(array $numArray, int $denom) : array {
+    $result = [];
+    
+    foreach ($numArray as $num){
+        try {
+            //echo 3;
+            $result[] = divide($num, $denom);
+            
+            //echo 4;
+        } catch (RuntimeException $exception) {
+            echo $exception->getMessage();
+            $result[] = $num;
+        }
+    }
+    return $result;
+}
