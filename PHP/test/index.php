451e513a66a0ae65aedd601b24f285069ae7b46d
jonatuxdr <jonathan.deriemaecker@gmail.com>
2018-10-23 10:07:44 +0200

    .gitignore test/ changed

---
+++ PHP/test/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="ISO-8859-1">
+        <title>Insert title here</title>
+    </head>
+    <body>
+    <h1>My home Page</h1>
+    
+    <p>Here is the time <?php echo (new DateTime())->format('Y-m-d H:i:s')?></p>
+    </body>
+</html>
